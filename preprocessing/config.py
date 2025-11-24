d879a7545142857c6b7b39a9a2ceeb9f83866995
{Narayana0804} <{lakshminarayanapakalapati28@gmail.com}>
2025-11-24 17:46:43 +0530

    updated code files of dataset, eda, preprocessing and preprocessed_data

---
+++ preprocessing/config.py
@@ -0,0 +1,11 @@
+# config.py
+
+DATASET_ROOT = r"D:\Desktop\DeepVision_Crowd\deepVision_crowd_monitor\Dataset\ShanghaiTech"
+OUT_ROOT     = r"D:\Desktop\DeepVision_Crowd\deepVision_crowd_monitor\preprocessed_data"
+
+RESIZE_SHORT_SIDE = 1024      
+EXTRACT_PATCHES = False  
+PATCH_SIZE = 512
+OVERLAP = 0.25
+SAVE_RESIZED_IMAGES = True
+K_NEIGHBORS = 3 
